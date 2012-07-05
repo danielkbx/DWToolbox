ae339b41bd67b6fb2c91a4e2e688031d8ffc0f37
Daniel Wetzel <daniel@keksbox.com>
2012-07-05 21:02:09 +0200

    Added xcode project

---
+++ dwToolbox/dwToolbox.h
@@ -0,0 +1,13 @@
+//
+//  dwToolbox.h
+//  dwToolbox
+//
+//  Created by Daniel Wetzel on 05.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dwToolbox : NSObject
+
+@end
---
+++ dwToolbox/dwToolbox.m
@@ -0,0 +1,13 @@
+//
+//  dwToolbox.m
+//  dwToolbox
+//
+//  Created by Daniel Wetzel on 05.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "dwToolbox.h"
+
+@implementation dwToolbox
+
+@end
